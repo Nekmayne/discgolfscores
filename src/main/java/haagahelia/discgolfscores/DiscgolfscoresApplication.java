98dee51b72d0523f7c73236b18da87a8784c8d6e
Nekmayne <bgm035@myy.haaga-helia.fi>
2021-04-08 09:28:06 +0300

    first commit

---
+++ src/main/java/haagahelia/discgolfscores/DiscgolfscoresApplication.java
@@ -0,0 +1,13 @@
+package haagahelia.discgolfscores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscgolfscoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscgolfscoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haagahelia/discgolfscores/DiscgolfscoresApplicationTests.java
@@ -0,0 +1,13 @@
+package haagahelia.discgolfscores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscgolfscoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
