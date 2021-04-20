735a9e7edf5762025d4983b63fd12ce68b9ebe32
Nekmayne <bgm035@myy.haaga-helia.fi>
2021-04-20 11:46:59 +0300

    heroku dep

--- src/main/java/haagahelia/discgolfscores/DiscgolfscoresApplication.java
+++ src/main/java/haagahelia/discgolfscores/DiscgolfscoresApplication.java
@@ -19,9 +19,10 @@ public class DiscgolfscoresApplication {
 	@Bean
 	public CommandLineRunner demo(CourseRepository repository, UserRepository uRepository) {
 		return (args) -> {
-
+			
 			User user1 = new User("user", "$2a$10$a1uN4cRSwIeIDj1l8Z1ZlOADz4CO4EuIa1rLT6zdgpbGyea4D9oa2", "email", "USER");
 			User user2 = new User("admin", "$2a$10$uWfXdetlWLrJFPTH4AHonOO2trGDDZfm7OVUnVyxgGs1C/YD56wHS", "email1", "ADMIN");
+			uRepository.deleteAll();
 			uRepository.save(user1);
 			uRepository.save(user2);
 
